4cc5c0a4910d901c7ad6934293a0958d70bb05aa
sasaplus1 <sasaplus1@gmail.com>
2021-03-20 22:44:14 +0900

    update GitHub Pages

--- docs/index.js
+++ docs/index.js
@@ -1,27 +1,33 @@
 (function(){
+
   'use strict';
 
-  var fragment = document.createDocumentFragment();
+  function onDOMContentLoaded() {
+    var fragment = document.createDocumentFragment();
 
-  var stylesheets = alternateStylesheets.getAlternateStylesheets();
-  var i, len, element, stylesheet;
+    var stylesheets = alternateStylesheets.getAlternateStylesheets();
+    var i, len, element, stylesheet;
 
-  for (i = 0, len = stylesheets.length; i < len; ++i) {
-    stylesheet = stylesheets[i];
+    for (i = 0, len = stylesheets.length; i < len; ++i) {
+      stylesheet = stylesheets[i];
 
-    element = document.createElement('option');
-    element.value = stylesheet.title;
-    element.innerHTML = stylesheet.title;
+      element = document.createElement('option');
+      element.value = stylesheet.title;
+      element.innerHTML = stylesheet.title;
 
-    fragment.appendChild(element);
+      fragment.appendChild(element);
+    }
+
+    var select = document.getElementById('js-select');
+
+    function onChange(event) {
+      alternateStylesheets.setAlternateStylesheet(event.target.value);
+    }
+
+    select.appendChild(fragment);
+    select.addEventListener('change', onChange, false);
   }
 
-  var select = document.getElementById('js-select');
+  document.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
 
-  function onChange(event) {
-    alternateStylesheets.setAlternateStylesheet(event.target.value);
-  }
-
-  select.appendChild(fragment);
-  select.addEventListener('change', onChange, false);
 }());
